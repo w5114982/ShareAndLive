9756f5e9b9c31a07b33e9ac5d52f60cde65ae95a
w5114982 <y5114982>
2017-05-03 16:30:51 +0800

    1、根据接口格式初步确定模型的字段

---
+++ WYShareAndLive/WYNewsModel.h
@@ -0,0 +1,13 @@
+//
+//  WYNewsModel.h
+//  WYShareAndLive
+//
+//  Created by MR-BusWang on 17/5/3.
+//  Copyright © 2017年 乾坤翰林. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYNewsModel : NSObject
+
+@end
---
+++ WYShareAndLive/WYNewsModel.m
@@ -0,0 +1,13 @@
+//
+//  WYNewsModel.m
+//  WYShareAndLive
+//
+//  Created by MR-BusWang on 17/5/3.
+//  Copyright © 2017年 乾坤翰林. All rights reserved.
+//
+
+#import "WYNewsModel.h"
+
+@implementation WYNewsModel
+
+@end
