625b84729682922bc6fba7ab69aabc23efb56635
w5114982 <y5114982>
2017-05-04 14:52:15 +0800

    1.新闻tableView中每个cell的model模型 2.http://c.m.163.com/nc/article/headline/T1348647853363/20-20.html（接口类型，及字段类型） T1348647853363 0 postid	"CJJD8R8A0001875P" hasCover	false hasHead	1 replyCount	9122 ltitle	"30万元保证金也不要了！杭州这些买房人反悔了" hasImg	1 digest	"还记得一个多月前，钱江晚报曾经报道过《不限购的法院拍卖房，最近有点“疯狂”》吗?3月中上旬那会儿，淘宝司法拍卖平台上，法拍房屡屡爆出高价，不少房源最后成交价都远" hasIcon	true docid	"CJJD8R8A0001875P" title	"30万元保证金也不要了！杭州这些买房人反悔了" order	1 priority	97 lmodify	"2017-05-04 12:25:17" boardid	"news_shehui7_bbs" ads 0 title	"看客：空巢青年 漂在城市的寻梦人" skipID	"3R710001|2252159" tag	"photoset" imgsrc	"http://cms-bucket.nosdn.127.net/c9f54c8e496642079292c9069d5961a720170504101950.jpeg" subtitle	"" skipType	"photoset" url	"3R710001|2252159" 1 title	"北京发布沙尘蓝色预警 城区遭沙尘笼罩" skipID	"00AP0001|2253392" tag	"photoset" imgsrc	"http://cms-bucket.nosdn.127.net/711ddb152087432bb0e77449bd9a77d220170504080336.jpeg" subtitle	"" skipType	"photoset" url	"00AP0001|2253392" 2 title	"厉害了大学生们！盘点高校吸金狂人" skipID	"00AP0001|2253384" tag	"photoset" imgsrc	"http://cms-bucket.nosdn.127.net/59631b76dca344fcb28dc62a9194c5e120170504074128.jpeg" subtitle	"" skipType	"photoset" url	"00AP0001|2253384" 3 title	"俄罗斯胜利日阅兵式彩排 民众驻足拍照" skipID	"00AO0001|2253427" tag	"photoset" imgsrc	"http://cms-bucket.nosdn.127.net/298cf177f3744394b5cfe6023339f15f20170504095139.jpeg" subtitle	"" skipType	"photoset" url	"00AO0001|2253427" 4 title	"这个村寄养126个孤残儿童 被父母遗弃" skipID	"00AP0001|2253415" tag	"photoset" imgsrc	"http://cms-bucket.nosdn.127.net/ee53bf90553e4ab09eac2c186c5df3f820170504092944.jpeg" subtitle	"" skipType	"photoset" url	"00AP0001|2253415" topic_background	"http://img2.cache.netease.com/m/newsapp/reading/cover1/C1348646712614.jpg" url_3w	"http://news.163.com/17/0504/11/CJJD8R8A0001875P.html" template	"normal1" votecount	8902 alias	"Top News" cid	"C1348646712614" url	"http://3g.163.com/news/17/0504/11/CJJD8R8A0001875P.html" hasAD	1 source	"钱江晚报" ename	"iosnews" subtitle	"" imgsrc	"http://cms-bucket.nosdn.127.net/3ea3f1198a82456f85c527b0b4ccd80f20170504114319.png" tname	"头条" ptime	"2017-05-04 11:43:30" 1 url_3w	"http://news.163.com/17/0504/03/CJII8E9H00018AOP.html" postid	"CJII8E9H00018AOP" votecount	0 replyCount	0 ltitle	"人民日报:与富二代官二代相比 寒门贵子贵在奋斗" digest	"近几年来，“寒门再难出贵子”的说法时常见诸舆论。在例如顶级高校农村娃比例渐少、招聘市场越发偏爱城市青年的报道中，人们似乎发现，尽管中国人口素质、教育水平有了巨大" url	"http://3g.163.com/news/17/0504/03/CJII8E9H00018AOP.html" docid	"CJII8E9H00018AOP" title	"人民日报:与富二代官二代相比 寒门贵子贵在奋斗" source	"人民网-人民日报" priority	97 lmodify	"2017-05-04 11:50:13" imgsrc	"http://cms-bucket.nosdn.127.net/0c00fca729d54df0b165a84463c284a320170504061704.png" subtitle	"" boardid	"news2_bbs" ptime	"2017-05-04 03:51:23"

--- WYShareAndLive/WYNewsModel.h
+++ WYShareAndLive/WYNewsModel.h
@@ -9,5 +9,42 @@
 #import <Foundation/Foundation.h>
 
 @interface WYNewsModel : NSObject
+/**新闻ID*/
+@property(nonatomic,copy)NSString *postid;
+/**webView界面*/
+@property(nonatomic,copy)NSString *url_3w;
+/**关注数*/
+@property(nonatomic,retain)NSNumber *votecount;
+/**跟帖数*/
+@property(nonatomic,retain)NSNumber *replyCount;
+/**新闻长标题*/
+@property (nonatomic,copy)NSString *ltitle;
+/**新闻简介*/
+@property(nonatomic,copy)NSString *digest;
+/***/
+@property(nonatomic,copy)NSString *url;
+/**postid*/
+@property(nonatomic,copy)NSString *docid;
+/**新闻标题*/
+@property(nonatomic,copy)NSString *title;
+/**新闻来源*/
+@property(nonatomic,copy)NSString *source;
+/**新闻时间*/
+@property(nonatomic,copy)NSString *Imodify;
+/***/
+@property(nonatomic,copy)NSString *priority;
+/**新闻类型标识*/
+@property(nonatomic,copy)NSString *boardid;
+/**新闻图片*/
+@property(nonatomic,copy)NSString *imgsrc;
+/***/
+@property(nonatomic,copy)NSString *subtitle;
+/**新闻发生的时间*/
+@property(nonatomic,copy)NSString *ptime;
+/***/
+@property(nonatomic,copy)NSString *TAG;
+/**专题新闻特有属性*/
+
+/**专题类型*/
 
 @end
